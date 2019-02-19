8947c96c3ec718964e852e7ea979addf31fdcaf4
sheadovas <sheadovas@gmail.com>
2019-02-19 20:27:31 +0100

    Create plugin

---
+++ README.md
@@ -0,0 +1,20 @@
+# List Symbols (v0.1b)
+Author: **sheadovas**
+
+## Description:
+A plugin that extracts symbols found inside binary with xrefs (like IDA subview imports/exports)
+
+## Features
+
+- [x] List symbols with xrefs.
+- [ ] Redirect xrefs urls to correct addresses in opened Binja.
+
+## Install
+
+```sh
+cd ~/.binaryninja && git clone https://github.com/sheadovas/binja-list-symbols
+```
+
+## License
+This plugin is released under a [MIT](LICENSE) license.
+
---
+++ __init__.py
@@ -0,0 +1,37 @@
+from binaryninja import *
+
+def list_symbols(bv,function):
+    fmt_xrefs = "- `{}`, **xrefs:** {}"
+    fmt_xref_el = "[{}+{:X}](binaryninja://?address={}) (`0x{:08X}`)"
+    md_text = "# Symbols\n\n"
+
+    def parse_symbol(refs, symbol):
+        xrefs = bv.get_code_refs(symbol.address)
+        if len(xrefs) == 0:
+            return ""
+        for r in xrefs:
+            refs.append(fmt_xref_el.format(
+                r.function.name, 
+                r.address - r.function.start,
+                r.address,
+                r.address))
+        return fmt_xrefs.format(symbol.full_name, ", ".join(refs))
+    
+    md = []
+    for name in bv.symbols:    
+        symbol = bv.symbols[name]
+        refs = []
+        if isinstance(symbol, list):
+            for s in symbol:
+                p = parse_symbol(refs, s)
+                if p not in md:
+                    md.append(p)
+        else:
+            md.append(parse_symbol(refs, symbol))
+    
+    md.sort()
+    md_text += "\n".join(md)
+    show_markdown_report("Symbols Listing", md_text)
+
+
+PluginCommand.register_for_address("List External Symbols", "List detected symbols", list_symbols)
